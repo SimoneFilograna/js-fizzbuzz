912549dedf34f4b4662c7fdf84219557804c361a
SimoneFilograna <simonefilogranaf12@gmail.com>
2023-07-04 15:56:51 +0200

    fixed

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
+const mainContainer = document.querySelector(".container");
+
 for( let i = 1; i <= 100; i++){    
     console.log(i)
 
-    const mainContainer = document.querySelector(".container");
     let boxContainer = document.createElement("div");
     boxContainer.classList.add("box", "text-center", "border", "d-flex","justify-content-center", "align-items-center", "fw-semibold", "fs-5", "bg-primary");
     mainContainer.append(boxContainer);
