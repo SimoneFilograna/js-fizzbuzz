3daaa17e0c09049d63ef06af6e250d405018e223
SimoneFilograna <simonefilogranaf12@gmail.com>
2023-07-04 15:15:41 +0200

    finished

---
+++ js/main.js
@@ -0,0 +1,27 @@
+for( let i = 1; i <= 100; i++){    
+    console.log(i)
+
+    const mainContainer = document.querySelector(".container");
+    let boxContainer = document.createElement("div");
+    boxContainer.classList.add("box", "text-center", "border", "d-flex","justify-content-center", "align-items-center", "fw-semibold", "fs-5", "bg-primary");
+    mainContainer.append(boxContainer);
+    
+    boxContainer.innerHTML = (i);
+
+
+    if (i % 3 === 0){
+        boxContainer.innerHTML = ("Fizz");
+        boxContainer.classList.add("bg-warning");
+    } 
+
+    if (i % 5 === 0){
+        boxContainer.innerHTML = ("Buzz");
+        boxContainer.classList.add("bg-success")
+    }
+
+    if (i % 3 === 0 && i % 5 === 0) {
+        boxContainer.innerHTML = ("FizzBuzz");
+        boxContainer.classList.add("bg-danger")       
+    }
+
+}
