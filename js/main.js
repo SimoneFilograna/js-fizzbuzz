4cc2baf158e3196482af9027aef38e6f6e29b891
SimoneFilograna <simonefilogranaf12@gmail.com>
2023-07-04 15:29:10 +0200

    if-fixed

--- js/main.js
+++ js/main.js
@@ -11,7 +11,12 @@ for( let i = 1; i <= 100; i++){
 
     if (i % 3 === 0){
         boxContainer.innerHTML = ("Fizz");
-        boxContainer.classList.add("bg-warning");
+        if (i % 5 === 0) {
+            boxContainer.innerHTML = ("Fizz Buzz");
+            boxContainer.classList.add("bg-danger") 
+        } else {
+            boxContainer.classList.add("bg-warning");
+        }
     } 
 
     if (i % 5 === 0){
@@ -19,9 +24,9 @@ for( let i = 1; i <= 100; i++){
         boxContainer.classList.add("bg-success")
     }
 
-    if (i % 3 === 0 && i % 5 === 0) {
-        boxContainer.innerHTML = ("Fizz Buzz");
-        boxContainer.classList.add("bg-danger")       
-    }
+    // if (i % 3 === 0 && i % 5 === 0) {
+    //     boxContainer.innerHTML = ("Fizz Buzz");
+    //     boxContainer.classList.add("bg-danger")       
+    // }
 
 }
